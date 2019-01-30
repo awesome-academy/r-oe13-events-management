81e1dc13b2d66204d38f6a340f04940e8caea8ef
iamPhong <truongvanphong97@gmail.com>
2019-01-30 21:38:34 +0700

    Create Login, Create Signup Pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
   private
 
   def set_locale
@@ -9,5 +9,13 @@ class ApplicationController < ActionController::Base
 
   def default_url_options
     {locale: I18n.locale}
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:email, :password, :password_confirmation, :remember_me, :name, :phone_number, :address, :avatar]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
